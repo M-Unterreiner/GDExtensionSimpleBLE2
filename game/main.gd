c81696f28c252316ae83e52ce76d3fe5a3fd3e1c
Martin Unterreiner <martin.unterreiner@igniti.de>
2024-01-02 18:05:52 +0100

    Read named peripheral now Renamed ble to ble_manager

--- game/main.gd
+++ game/main.gd
@@ -2,7 +2,9 @@ extends Node
 
 var ble_manager : GDExtensionSimpleBLE
 var data
-var peripherals : Array = ["2_Device"]  
+var device_2 : String = "2_Device"
+var peripherals : Array = [device_2]
+ 
 func _ready() -> void:
 	ble_manager = GDExtensionSimpleBLE.new()
 	ble_manager.print_message.connect(_print_message)
@@ -12,7 +14,7 @@ func _ready() -> void:
 	
 func _process(delta):
 	while(true):
-		var data : PackedByteArray= ble_manager.readPeripheral()
+		var data : PackedByteArray= ble_manager.readPeripheral(device_2)
 		var quat = string_to_quaternion(data.get_string_from_ascii())
 		if(quat != Quaternion.IDENTITY):
 			print(quat)
