8c0ea0bb25215524de4ebdd52f8f523cbe814918
Martin Unterreiner <martin.unterreiner@igniti.de>
2024-01-10 16:45:33 +0100

    Deleted obsolete lines of code

--- game/main.gd
+++ game/main.gd
@@ -8,18 +8,7 @@ var peripherals : Array = [device_2]
 func _ready() -> void:
 	ble_manager = GDExtensionSimpleBLE.new()
 	ble_manager.print_message.connect(_print_message)
-	ble_manager.connectPeripherals(peripherals)
-	ble_manager.connectService()
 	
-	
-func _process(delta):
-	while(true):
-		var data : PackedByteArray= ble_manager.readPeripheral(device_2)
-		var quat = string_to_quaternion(data.get_string_from_ascii())
-		if(quat != Quaternion.IDENTITY):
-			print(quat)
-
-
 func string_to_quaternion(data : String):
 	if (data.length() > 0):
 		var sArray : PackedFloat64Array = data.split_floats(",",false)
