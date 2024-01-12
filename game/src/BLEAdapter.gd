27806f7db24e7f4c1f737d068c3c13486e6980ca
Martin Unterreiner <martin.unterreiner@igniti.de>
2024-01-12 20:03:13 +0100

    Devices are now listed

--- game/src/BLEAdapter.gd
+++ game/src/BLEAdapter.gd
@@ -7,6 +7,6 @@ func _ready():
 func _on_btn_start_scan_button_down():
 	start_scan(2500)
 
-
-func _on_found_new_peripheral(message):
-	print(message)
+#
+#func _on_found_new_peripheral(message):
+	#print(message)
