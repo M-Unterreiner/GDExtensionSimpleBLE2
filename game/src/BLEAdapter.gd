2d6fc4db80daf60e4bc08dbada94fc605009a11c
Martin Unterreiner <martin.unterreiner@igniti.de>
2024-01-12 20:38:01 +0100

    Item is now selectable

--- game/src/BLEAdapter.gd
+++ game/src/BLEAdapter.gd
@@ -16,3 +16,9 @@ func _on_btn_start_scan_button_down():
 
 func _on_spin_box_value_changed(ms):
 	ms_to_scan = ms
+
+
+func _on_item_list_item_selected(index):
+	var item_list : ItemList = get_node("../BLE GUI/VBoxContainer/ItemList")
+	print("Item ", item_list.get_item_text(index) ," selected")
+	
