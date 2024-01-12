8e28f3f727c7c6bdcd12b05b82638d5917915185
Martin Unterreiner <martin.unterreiner@igniti.de>
2024-01-12 16:11:42 +0100

    Started implementing receiving adapters

--- game/src/BLEAdapter.gd
+++ game/src/BLEAdapter.gd
@@ -2,3 +2,11 @@ extends BLEAdapter
 
 func _ready():
 	var adapterList = getAdapterList()
+
+
+func _on_btn_start_scan_button_down():
+	pass # Replace with function body.
+
+
+func _on_btn_stop_scan_button_down():
+	pass # Replace with function body.
