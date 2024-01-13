1c8668e3c600958fd768259eec77b379c852698c
Martin Unterreiner <martin.unterreiner@igniti.de>
2024-01-13 13:58:35 +0100

    Seleciton of peripherals is now possible

--- game/src/ItemList.gd
+++ game/src/ItemList.gd
@@ -8,3 +8,9 @@ extends ItemList
 
 func _on_ble_adapter_started_scan():
 	clear()
+
+
+func _on_ble_peripheral_manager_new_peripheral_stored():
+	var peripheral_manager : Node = get_node("../../../ble_peripheral_manager")
+	for peripheral in peripheral_manager.peripherals_:
+		add_item(peripheral.identifier())
--- game/src/ble_peripheral_manager.gd
+++ game/src/ble_peripheral_manager.gd
@@ -2,21 +2,34 @@ extends Node
 
 ## This node is responsible for holding all the BLEPeripherals
 var peripherals_ : Array = []
+var selected_peripheral_ : BLEPeripheral = null
 
 signal new_peripheral_stored
 signal cleared_stored_peripherals
 
 func _on_ble_adapter_found_new_peripheral(id):
 	var new_peripheral : BLEPeripheral = instance_from_id(id)
-	print("Created new peripheral with id:", new_peripheral.get_instance_id())
+	print_debug("Created new peripheral with id:", new_peripheral.get_instance_id())
 	store_new_peripheral(new_peripheral)
 	
 func store_new_peripheral(new_peripheral : BLEPeripheral):
 	peripherals_.append(new_peripheral)
-	print(new_peripheral.identifier())
+	print_debug(new_peripheral.identifier(), ":", new_peripheral.get_instance_id())
 	emit_signal("new_peripheral_stored")
 
 
 func _on_ble_adapter_started_scan():
 	peripherals_.clear()
 	emit_signal("cleared_stored_peripherals")
+
+
+func _on_item_list_item_selected(index):
+	var item_list : ItemList = get_node("../BLE GUI/VBoxContainer/ItemList")
+	for peripheral : BLEPeripheral in peripherals_:
+		var id = peripheral.get_instance_id()
+		print_debug(peripheral.identifier(),":",id," was selected")
+		
+		#if(peripheral.identifier() == item_list.get_item_text(index)):
+
+			#selected_peripheral_ = instance_from_id(id)
+			#print_debug(selected_peripheral_.identifier() ," was selected.")
