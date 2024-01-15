dca19a2da77a8e2026e6dce8739912320c3a5730
Martin Unterreiner <martin.unterreiner@igniti.de>
2024-01-15 19:04:58 +0100

    Implemented function for receiving services

--- game/src/ListServices.gd
+++ game/src/ListServices.gd
@@ -1,10 +1,13 @@
 extends ItemList
 
 
-
-func connect_service_to_signals(service : BLEPeripheral):
-	print_debug("Connect ", peripheral.identifier(), " to signals.")
+func connect_peripheral_to_signals(peripheral : BLEPeripheral):
+	print_debug("Connect ", peripheral.identifier(), " to service signals.")
 	#if (peripheral.is_connected("peripheral_is_connected", _on_peripheral_is_connected)):
-	peripheral.connect("peripheral_is_connected", _on_peripheral_is_connected)
+	peripheral.connect("services_send", _on_service_send)
 	#if (peripheral.is_connected("peripheral_is_disconnected", _on_peripheral_is_disconnected)):
-	peripheral.connect("peripheral_is_disconnected", _on_peripheral_is_disconnected)
+	#peripheral.connect("peripheral_is_disconnected", _on_peripheral_is_disconnected)
+
+func _on_service_send():
+	print_debug("Services were emitted and received")
+
