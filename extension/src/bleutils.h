b422bae79f180c14dd23cf381dd5109228b2873c
Martin Unterreiner <martin.unterreiner@igniti.de>
2024-01-18 15:17:41 +0100

    Added pragma once

--- extension/src/bleutils.h
+++ extension/src/bleutils.h
@@ -1,5 +1,4 @@
-#ifndef GDBLEUTILS_CLASS_H
-#define GDBLEUTILS_CLASS_H
+#pragma once
 
 #include <godot_cpp/classes/ref.hpp>
 #include <simpleble/SimpleBLE.h>
@@ -11,4 +10,3 @@ class BLEUtils {
 public:
   static PackedByteArray stringToByteArray(const SimpleBLE::ByteArray &p_bytes);
 };
-#endif // GDBLEUTILS_CLASS_H
