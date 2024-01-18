d57ab9e8af7ebaf79ace5d4580b58f12c0968199
Martin Unterreiner <martin.unterreiner@igniti.de>
2024-01-18 19:12:30 +0100

    Deleted utility script

--- gdextensionsimpleble/src/utility.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-class utility:
-	
-	func string_to_quaternion(data : String) -> Quaternion:
-		if (data.length() > 0):
-			var sArray : PackedFloat64Array = data.split_floats(",",false)
-			var quat : Quaternion = Quaternion(sArray[0],sArray[1],sArray[2],sArray[3])
-			return quat
-		return Quaternion.IDENTITY
